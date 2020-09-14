180b6bf3c6b2bcf841c27d86a9011eb2c6825815
Krystian Jarmicki <jarmicki@gmail.com>
2020-09-14 17:07:21 +0200

    cleanup, automate releases based on versioning

--- index.ts
+++ index.ts
@@ -1,1 +1,4 @@
-export * from './mongodb-schema-document-validator';
+export {MongodbSchemaDocumentValidator} from './mongodb-schema-document-validator';
+export function isDocumentFailedValidationError(error: Error & {code?: number}) {
+  return error.code === 121;
+}
